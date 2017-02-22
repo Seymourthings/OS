595cd224bd355e58fb44956dd598f9d8fa09f0e2
Desean Abraham <deseanab@buffalo.edu>
2017-02-22 22:34:58 +0000

    forgot to add the new files where sys_write and fil_table acutally is defined

---
+++ kern/include/file_syscall.h
@@ -0,0 +1,26 @@
+#ifndef _FILE_SYSCALL_H_
+#define _FILE_SYSCALL_H_
+
+#include <synch.h>
+#include <thread.h>
+#include <vnode.h>
+
+/* File Handle & File System Syscalls */
+
+struct file_handle {
+        struct vnode *vnode;    /* actual file on disk */
+
+        struct lock *lock;      /* synchronization for file */
+
+        struct semaphore *sem;  /* Might use this for sys_read */
+
+        int count;              /* number of processes pointing to file */
+
+        unsigned int flags;
+
+        off_t offset;
+};
+
+int sys_write(int fd, void *buf, size_t buflen, int32_t *retval);
+
+#endif /*_FILE_SYSCALL_H_ */
