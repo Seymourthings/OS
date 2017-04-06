e9030c1c5f2472a382641f3a87f5adbc534f7c77
Adam Kotler <adamkotler@gmail.com>
2017-04-06 00:08:26 +0000

    trying to revert

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -84,9 +84,10 @@ syscall(struct trapframe *tf)
 	int err;
  	
 	/* Lseek vars */
-	off_t offset = 0;
-	off_t err_ret = 0;
+//	off_t offset = 0;
+//	off_t err_ret = 0;
 
+	int64_t offset;
 
 	KASSERT(curthread != NULL);
 	KASSERT(curthread->t_curspl == 0);
@@ -140,14 +141,8 @@ syscall(struct trapframe *tf)
 	    	break;*/
     
 	    case SYS_lseek:
-	   	err_ret = sys_lseek((int)tf->tf_a0,(((off_t)tf->tf_a2) << 32)+tf->tf_a3, 
-				   (const_userptr_t)tf->tf_sp+16, &offset);
-	    	err = err_ret << 32; //higher 32 bits
-	    	if(!err){
-	    		tf->tf_v1 = (int32_t)offset;
-	    	}
-	    	retval = (int32_t)(offset >> 32); //lower 32 bits
-
+	   	err = sys_lseek(tf->tf_a0, (off_t) ((off_t)tf->tf_a2 << 32 | (off_t)tf->tf_a3), (const_userptr_t)tf->tf_sp+16, &offset);
+	    	retval = offset >> 32; //lower 32 bits
 	    	break;
 
 	    case SYS_dup2:
